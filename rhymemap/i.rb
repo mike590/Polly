f334e0589d53d34a4d83ee8a83a4a7531b9da4a4
mike590 <mike.gherard@gmail.com>
2015-04-24 12:07:49 -0400

    Completed rhyme map for i

--- rhymemap/i.rb
+++ rhymemap/i.rb
@@ -14,17 +14,57 @@
 
 [
   {pron: "ˈif", ex: "shift"},
-  {pron: "ˌif", ex: "face-lift"}
+  {pron: "ˌif", ex: "face-lift"},
+  {pron: "ˈip", ex: "ship"},
+  {pron: "ˌip", ex: "leadership"}
 ]
 
 [
   {pron: "ˈiks", ex: "mix"},
   {pron: "ˌiks", ex: "politics"},
-  {pron: "iks", ex: "matrix"}
+  {pron: "iks", ex: "matrix"},
+  {pron: "ˈis", ex: "miss"},
+  {pron: "ˌis", ex: "can't-miss"},
+  {pron: "is", ex: "discover"}
 ]
 
 [
   {pron: "ˈild", ex: "build"},
   {pron: "ˈilm", ex: "film"},
-  {pron: "ˈil", ex: "bill"}
+  {pron: "ˈil", ex: "bill"},
+  {pron: "ˌil", ex: "winter-kill"}
+]
+
+[
+  {pron: "ˈim", ex: "simple"},
+  {pron: "im", ex: "employee"},
+  {pron: "ˈin", ex: "sin"},
+  {pron: "ˌin", ex: "individual"},
+  {pron: "in", ex: "indeed"}
+
+[
+  {pron: "ˈiŋ", ex: "bring"},
+  {pron: "ˌiŋ", ex: "everything"},
+  {pron: "iŋ", ex: "seasoning"}
+]
+
+[
+  {pron: "ˈir", ex: "ear"},
+  {pron: "ˌir", ex: "dog-eared"}
+]
+
+[
+  {pron: "ˈith", ex: "with"},
+  {pron: "ˈiv", ex: "live"},
+  {pron: "ˌiv", ex: "give-and-go"},
+  {pron: "iv", ex: "active"},
+  {pron: "ˌiz", ex: "his"},
+  {pron: "iz", ex: "his"},
+  {pron: "ˈiz", ex: "his"}
+]
+
+[
+  {pron: "ˈit", ex: "hit"},
+  {pron: "ˌit", ex: "benefit"},
+  {pron: "it", ex: "credit"}
 ]
